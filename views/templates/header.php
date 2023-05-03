0da4c1bdd60e1acd73172544fe89288f2de6417c
Toine2687 <ant.petit@pm.me>
2023-05-03 22:56:38 +0200

    page photographe ok

--- views/templates/header.php
+++ views/templates/header.php
@@ -21,18 +21,5 @@
 
 <body>
     <header>
-        <nav>
-            <a href="/"><img id="logo" src="../../public/assets/img/logo_APP.png" alt="logo photographe amiens"></a>
-
-            <div id="navLinks">
-                <ul id="menu">
-                    <li class="menuItem"><a href="/controllers/portfolioCtrl.php">Portfolio</a></li>
-                    <li class="menuItem"><a href="/controllers/photographeCtrl.php">Photographe</a></li>
-                    <li class="menuItem"><a href="/controllers/blogCtrl.php">Journal</a></li>
-                    <li class="menuItem"><a href="/controllers/contactCtrl.php">Contact</a></li>
-                    <li class="menuItem"><a href="/controllers/clientsCtrl.php">Clients</a></li>
-                </ul>
-            </div>
-            <i id="burger" class="fa-solid fa-bars"></i>
-        </nav>
+        <?php include __DIR__ . '/navBar.php'; ?>
     </header>
